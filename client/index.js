bbe19db7f193b79089e2f0b56fde36f80f555664
Austin Jones <122324609+Jones-Austin@users.noreply.github.com>
2023-03-27 19:56:27 -0400

    got notif working

---
+++ client/index.js
@@ -0,0 +1,33 @@
+const client = (() => {
+    let serviceWorkerRegObj = undefined;
+    const checkNotifSupport = () => {
+        if (!("Notification" in window)) {
+            return Promise.reject("Notification is not supported")
+        }
+        console.log("The browser supports notification")
+        return Promise.resolve("ok")
+    }
+
+    const registerServiceWorker = () => {
+        if (!("serviceWorker" in navigator)) {
+            return Promise.reject("service worker is not available")
+        }
+        return navigator.serviceWorker.register("service_worker.js")
+            .then(regObj => {
+                console.log("service worker is registered")
+                serviceWorkerRegObj = regObj;
+            })
+    }
+
+    const requestNotificationPermission = () => {
+        return Notification.requestPermission(status => {
+            console.log("Notification permission status:", status)
+        })
+    }
+
+    // how constructor is called 
+    checkNotifSupport()
+        .then(registerServiceWorker)
+        .catch(err => console.error(err))
+        .then(requestNotificationPermission)
+})();
